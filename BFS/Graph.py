79ded32c90616a893db8b7dc71911a603d4e9bd1
hoodieman0 <hoodieman8@gmail.com>
2023-02-21 10:35:54 -0500

    edited Graph class to use dicts

--- BFS/Graph.py
+++ BFS/Graph.py
@@ -14,46 +14,51 @@ class Node:
             exit(101)
         self.neighbors.append(node)
 
+        
+"""
+Graph is a dict of coordinates. The values are the distances between the nodes
+Functions:
+Get Graph
+string to Graph
+Connect Graph
+Movement operations
+"""
+
 
 class Graph:
-    start: Node
-    collection = [Node]
-    height: int
-    width: int
+    isDirected = False
 
-    def __init__(self, height: int, width: int) -> None:
-        self.height = height
-        self.width = width
+    # This dict must follow the format dict{node: dict{connectedNode: distanceInt}}
+    # The current use case has nodes as a tuple: (xInt, yInt)
+    graphDict = {}
 
-    def AddNeighbors(self):
-        return
+    def __init__(self, graphDict: dict, isDirected: bool) -> None:
+        self.isDirected = isDirected
+        self.graphDict = graphDict
+        if not self.isDirected:
+            self.MakeUndirected()
+
+    # In graphDict, sets the value of key the node1 to a tuple (node2, distance)
+    def ConnectOneWay(self, node1, node2, distance) -> None:
+        self.graphDict.setdefault(node1, {})[node2] = distance
     
-    def CreateGraph(self, graph: str):
-        #9x9
+    # For all dict keys, take their connected node and connect that node to the key
+    # Eliminates the need for the input dict to have all possibilities
+    def MakeUndirected(self) -> None:
+        for node1 in list(self.graphDict.keys()):
+            for (node2, distance) in self.graphDict[node1].items():
+                self.ConnectOneWay(node2, node1, distance)
 
-        def isValidNeighbor(d: dict, x: int, y: int):
-            if d[(x,y)]:
-                return True
-            else:
-                return False
+    # Returns a list of all the nodes in the graph
+    def ListNodes(self) -> list:
+        s1 = set([k for k in self.graphDict.keys()])
+        s2 = set([k2 for v in self.graphDict.values() for k2, v2, in v.items()])
+        nodes = s1.union(s2)
+        return list(nodes)
 
-        d = {}
-        j = 0
-        for i in range (0, self.width*self.height):
-            if graph[i] == '.':
-                node = Node((i+1, j+1))
-                for x in range(0, 2):
-                    for k in range(0,2):
-                        if isValidNeighbor(d, i+x, j+y):
-                            node.SetNeighbor(d[(x,y)])
-                
-                d[(i+1,j+1)] = node
-                j = j + 1
-
-    
-            
-
-
+    # Returns the connected relationship graph
+    def GetGraph(self) -> dict:
+        return self.graphDict
         
 """
 -----.-----
