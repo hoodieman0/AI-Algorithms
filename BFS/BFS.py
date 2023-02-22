0f3a5b4dbd669f83f33b01a6c02929fa3e8f2c26
hoodieman0 <hoodieman8@gmail.com>
2023-02-22 14:34:43 -0500

    created CoordinateBFS() search, added BFS base class

--- BFS/BFS.py
+++ BFS/BFS.py
@@ -1,4 +1,52 @@
 from Graph import Graph, CoordinateNode, DictConstructor
+from abc import ABC, abstractmethod
+
+# Breadth First Search Base Class
+class BFS(ABC):
+    def __init__(self, graph, start, goal) -> None:
+        self.graph = graph
+        self.start = start
+        self.goal = goal
+    
+    @abstractmethod
+    def SearchGraph():
+        pass
+
+# BFS using (x, y) coordinate system
+class CoordinateBFS(BFS):
+    def SearchGraph(self):
+        queue = []
+        visited = []
+
+        queue.append(self.start)
+        visited.append(self.start)
+        i = 1
+        print(str(i) + ". Frontier: " + str(queue))
+        i = i + 1
+
+        while queue:
+            node = queue.pop(0)
+            print("\n@("+ str(node[0])+", " + str(node[1]) + ")")
+            #visited.append(node)
+
+            if node == self.goal:
+                print("Hey! I reached the goal!")
+                return True
+            
+            for key in self.graph[node].keys():
+                if not key in visited:
+                    queue.append(key)
+                    visited.append(key)
+                
+
+            print(str(i) + ". Frontier: " + str(queue))
+            print(str(i) + ". Visited: " + str(visited))
+            i = i + 1
+        
+        return False
+
+
+
 
 maze = "-----.-----\n-.........-\n--.-----.--\n-.........-\n-----.-----\n-----.-----\n"
 file = open("output.txt", "w")
@@ -37,15 +85,14 @@ for key in graph.keys():
     if (key[0], key[1] + 1) in graph:
         graph[key][key[0], key[1] + 1] = 1
 
-print(graph)
+#print(graph)
 
 
 myGraph = Graph(graph, False)
+print(CoordinateBFS(myGraph.GetGraph(), (6,1), (6,6)))
 file.close()
 
 
-# Breadth First Search
-#
-class BFS:
+
 
 
