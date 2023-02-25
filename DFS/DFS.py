72d91bfc982171b9d21352627a68eb03ab5ff3eb
hoodieman0 <hoodieman8@gmail.com>
2023-02-25 00:29:51 -0500

    added CoordinateDFS class

--- DFS/DFS.py
+++ DFS/DFS.py
@@ -7,3 +7,33 @@ class DFS(ABC):
     @abstractmethod
     def SearchGraph():
         pass
+
+class CoordinateDFS(DFS):
+    def SearchGraph(self, start: tuple, goal: tuple) -> bool:
+        stack = []
+        visited = []
+
+        stack.append(start)
+        visited.append(start)
+
+        i = 1
+        print(str(i) + ". Frontier: " + str(stack))
+        i = i + 1
+
+        while stack:
+            node = stack.pop()
+
+            if node == goal:
+                print("Hey! I reached the goal!")
+                return True
+            
+            for key in self.graph[node].keys():
+                if not key in visited:
+                    stack.append(key)
+                    visited.append(key)
+
+            print(str(i) + ". Frontier: " + str(stack))
+            print(str(i) + ". Visited: " + str(visited))
+            i = i + 1
+
+        return False
