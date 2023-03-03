fe9384583545d5906194bfde72a08adf4bd8be3c
hoodieman0 <hoodieman8@gmail.com>
2023-03-03 15:35:42 -0500

    updated main.py to use MazeGenerator

--- BFS/main.py
+++ BFS/main.py
@@ -3,33 +3,40 @@ import turtle
 import maze
 from DrawBFS import DrawCoordinateBFS, Direction
 from Graph import Graph, DictConstructor, GetMazeFromFile, MakeCoordinateNodes, ConnectAdjacentNodes
-
-# Drawing the maze
-myPen=turtle.Turtle()
-myPen.penup()
-myPen.goto(20,-180)
-myPen.pendown()
-myPen.shape('turtle')
-myPen.color("#DB148E")
-myPen.width(5)
-myPen.left(90)
-
+from MazeGenerator import MazeGenerator
 # ==========================================================
 # main.py
 # Author: James Mok
 # Created On: 20 Feb 2023
 
-# Start of maze movement
 
-filename = "maze.txt"
-start = (19,1)
-end = (19,41)
+# Global settings
 
+filename = "maze2.txt"
+start = (8, 1)
+end = (8, 15)
+mazeColor = "#00c8ff"
+playerColor = "red"
+
+# Transform file into a list
 mazeList = GetMazeFromFile(filename)
+
+
+# Generate maze drawing with player
+
+m = MazeGenerator(mazeList)
+m.DrawMaze(mazeColor)
+myPen = m.DrawPlayer(start, playerColor)
+
+
+# Create Graph
 
 coordList = MakeCoordinateNodes(mazeList)
 graph = ConnectAdjacentNodes(DictConstructor(coordList))
 myGraph = Graph(graph, False)
+
+
+# Start of maze movement
 
 search = DrawCoordinateBFS(myGraph.GetGraph())
 print(search.GetPath(start, end))
